dd6732a28f66de4b835a8cc0374c7d2a559d578f
Armand Delessert <armand.delessert@gmail.com>
2018-12-11 17:43:48 +0100

    Reprise du projet. 🤯

--- TestUWP/MainPage.xaml.cs
+++ TestUWP/MainPage.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿//using Microsoft.Toolkit.Uwp.UI.Animations;
+using System;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using Windows.ApplicationModel.Resources;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.Graphics.Display;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +19,85 @@ namespace TestUWP
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Infos système d'exploitation
+            string caca;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                caca = "Yes";
+            }
+            else
+            {
+                caca = "No";
+            }
+
+            //var osVersion = Environment.OSVersion;
+
+            // Infos hardware
+
+            // Récupération des informations sur l'écran
+            Size screenSize_Pixels = new Size
+            {
+                Width = DisplayInformation.GetForCurrentView().ScreenWidthInRawPixels,
+                Height = DisplayInformation.GetForCurrentView().ScreenHeightInRawPixels
+            };
+
+            double scaleFactor = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+
+            float rawDpiX = DisplayInformation.GetForCurrentView().RawDpiX;
+            float rawDpiY = DisplayInformation.GetForCurrentView().RawDpiY;
+            ResolutionScale resolutionScale = DisplayInformation.GetForCurrentView().ResolutionScale;
+            DisplayOrientations displayNativeOrientation = DisplayInformation.GetForCurrentView().NativeOrientation;
+            DisplayOrientations displayCurrentOrientation = DisplayInformation.GetForCurrentView().CurrentOrientation;
+
+            ResourceLoader rl = ResourceLoader.GetForCurrentView();
+
+            ScreenInfos.Text = rl.GetString("ScreenInfos_Size/Text") + screenSize_Pixels.Width + " × " + screenSize_Pixels.Height;
+            ScreenInfos.Text +=
+                Environment.NewLine + rl.GetString("ScreenInfos_PpiX/Text") + rawDpiX +
+                Environment.NewLine + rl.GetString("ScreenInfos_PpiY/Text") + rawDpiY;
+
+            ScreenInfos.Text += Environment.NewLine +
+                rl.GetString("ScreenInfos_Scale/Text") + resolutionScale;
+
+            ScreenInfos.Text += Environment.NewLine +
+                rl.GetString("ScreenInfos_NativeOrientation/Text") + displayNativeOrientation;
+            ScreenInfos.Text += Environment.NewLine +
+                rl.GetString("ScreenInfos_CurrentOrientation/Text") + displayCurrentOrientation;
+
+            // Connexion réseau/Internet
+            bool isNetworkAvailable = NetworkInterface.GetIsNetworkAvailable();
+
+            NetworkInfos.Text = rl.GetString("NetworkInfos_IsAvailable/Text") + isNetworkAvailable;
+
+            // Autres infos
+            TimeSpan timeSpendSinceStart = getTimeSpendSinceStart(); // Jours, heures, minutes, secondes, millisecondes
+            DateTime startDate = DateTime.Now - timeSpendSinceStart; // Année, mois, jours, heures, minutes, secondes, millisecondes
+
+            OtherInfos.Text =
+                rl.GetString("NetworkInfos_StartDate/Text") +
+                startDate +
+                Environment.NewLine +
+                rl.GetString("NetworkInfos_TimeElapsedSinceStart/Text") +
+                timeSpendSinceStart.ToString();
+        }
+
+        private TimeSpan getTimeSpendSinceStart()
+        {
+            return new TimeSpan(0, 0, 0, 0, Environment.TickCount); // Jours, heures, minutes, secondes, millisecondes
+        }
+
+        private String timeSpendSinceStart_String()
+        {
+            TimeSpan t = getTimeSpendSinceStart();
+
+            return
+                "System started since " +
+                t.Days + " days, " +
+                t.Hours + " hours, " +
+                t.Minutes + " minutes, " +
+                t.Seconds + " seconds, " +
+                t.Milliseconds + " milliseconds.";
         }
     }
 }
